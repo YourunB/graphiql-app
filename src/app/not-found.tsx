920acb0126627ec750dde008458e61edc4e513c3
yourunb <bxyura@gmail.com>
2024-08-20 13:27:48 +0300

    feat: add page 404 to app

---
+++ src/app/not-found.tsx
@@ -0,0 +1,16 @@
+import s from './not-found.module.css'
+import Link from 'next/link';
+
+export default function PageNotFound() {
+  return (
+    <main className={s.page}>
+      <div className={s['error-box']}>
+        <h2 className={s['error-box__title']}>404</h2>
+        <p className={s['error-box__description']}>This Page Not Found</p>
+        <Link className={s['error-box__link']} href="/">
+          HOME
+        </Link>
+      </div>
+    </main>
+  );
+}
