22265e7a250843a2aedfd0a6830bca597ec47ab1
yourunb <bxyura@gmail.com>
2024-08-20 13:18:01 +0300

    refactor: change tyle for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import { metadata } from './layout';
+import s from './page.module.css';
 
 metadata.title = 'Home page';
 metadata.description = 'Home page GraphiQl App';
 
 export default function Home() {
   return (
-    <main>
+    <main className={s.page}>
       Home page...
     </main>
   );
