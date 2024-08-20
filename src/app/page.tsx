fb2870e2cf084f2ca37ca8d0adc308cd5d26a23e
yourunb <bxyura@gmail.com>
2024-08-20 13:22:05 +0300

    refactor: rename Home by PageHome

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import s from './page.module.css';
 metadata.title = 'Home page';
 metadata.description = 'Home page GraphiQl App';
 
-export default function Home() {
+export default function PageHome() {
   return (
     <main className={s.page}>
       Home page...
