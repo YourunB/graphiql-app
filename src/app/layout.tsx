1842a4a5ae21185d2cff532c7f501c997e722e0c
yourunb <bxyura@gmail.com>
2024-08-20 13:14:05 +0300

    feat: add footer to app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import GraphAnimation from './components/GraphAnimation';
+import Footer from './components/Footer';
 
 export const metadata: Metadata = {
   title: 'GraphiQL App',
@@ -18,6 +19,7 @@ export default function RootLayout({
       <body>
         <GraphAnimation/>
         {children}
+        <Footer />
       </body>
     </html>
   );
