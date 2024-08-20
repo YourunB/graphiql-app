c17acbe0999717470b0bc4fe537d37fc71bcaabf
yourunb <bxyura@gmail.com>
2024-08-20 11:33:21 +0300

    feat: add esLint and vitest tests to project

---
+++ vitest.config.ts
@@ -0,0 +1,21 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    setupFiles: '/test/setup.ts',
+    css: true,
+    globals: true,
+    coverage: {
+      include: [
+        'src/**/*.ts',
+        'src/**/*.tsx'
+      ],
+      exclude: [
+        'src/app/state/**'
+      ],
+    },
+  },
+});
