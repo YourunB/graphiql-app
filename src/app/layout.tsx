a73b4d5459abf0a042f278368c01b86766ffd367
yourunb <bxyura@gmail.com>
2024-08-20 13:07:12 +0300

    feat: add GraphAnimation component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable react-refresh/only-export-components */
 import type { Metadata } from 'next';
 import './globals.css';
+import GraphAnimation from './components/GraphAnimation';
 
 export const metadata: Metadata = {
   title: 'GraphiQL App',
@@ -14,7 +15,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <GraphAnimation/>
+        {children}
+      </body>
     </html>
   );
 }
