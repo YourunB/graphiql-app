0304c2afb9dc4c2684bc5f4f06717eaa03f43337
Lilo002 <lisabasarab@gmail.com>
2024-08-21 13:48:54 +0300

    feat: add registerPage form

---
+++ src/app/components/forms/RegisterForm.tsx
@@ -0,0 +1,96 @@
+'use client';
+import s from './RegisterForm.module.css';
+
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { registerSchema } from '@/app/utils/validation';
+import Link from 'next/link';
+import { registerWithEmailAndPassword } from '@/firebase';
+import { RegisterData } from '@/app/types';
+
+export default function RegisterForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm({
+    resolver: yupResolver(registerSchema),
+    mode: 'onChange',
+  });
+
+  const submitFrom = async (data: RegisterData) => {
+    try {
+      await registerWithEmailAndPassword(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <form className={s['register__form']} onSubmit={handleSubmit(submitFrom)}>
+        <div className={s['register__content']}>
+          <div className={s['register__field']}>
+            <label htmlFor="name" className={s['register__label']}>
+              Name:
+            </label>
+            <input type="text" className={s['register__input']} placeholder="Name" id="name" {...register('name')} />
+          </div>
+          {errors.name && <div className={s.error}>{errors.name.message}</div>}
+        </div>
+        <div className={s['register__content']}>
+          <div className={s['register__field']}>
+            <label htmlFor="email" className={s['register__label']}>
+              Email:
+            </label>
+            <input
+              type="text"
+              className={s['register__input']}
+              placeholder="E-mail Address"
+              id="email"
+              {...register('email')}
+            />
+          </div>
+          {errors.email && <div className={s.error}>{errors.email.message}</div>}
+        </div>
+        <div className={s['register__content']}>
+          <div className={s['register__field']}>
+            <label htmlFor="password" className={s['register__label']}>
+              Password:
+            </label>
+            <input
+              type="password"
+              className={s['register__input']}
+              placeholder="Password"
+              id="password"
+              {...register('password')}
+            />
+          </div>
+          {errors.password && <div className={s.error}>{errors.password.message}</div>}
+        </div>
+        <div className={s['register__content']}>
+          <div className={s['register__field']}>
+            <label htmlFor="confirmPassword" className={s['register__label']}>
+              Confirm password:
+            </label>
+            <input
+              type="password"
+              className={s['register__input']}
+              placeholder="Confirm password"
+              id="confirmPassword"
+              {...register('confirmPassword')}
+            />
+          </div>
+          {errors.confirmPassword && <div className={s.error}>{errors.confirmPassword.message}</div>}
+        </div>
+
+        <div className={s['register__btns']}>
+          <button disabled={!isValid} className={s['register__btn']} type="submit">
+            Register
+          </button>
+        </div>
+        <div className={s['register__link']}>
+          Already have an account? <Link href="/login">Login</Link> now.
+        </div>
+      </form>
+  )
+}
