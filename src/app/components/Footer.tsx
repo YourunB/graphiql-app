376741d9f66e5af9017a04100c7ae27985456ea3
valeryaosta <gerasimenko.valery@gmail.com>
2024-08-22 01:56:23 +0300

    feat: add footer component

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,5 +1,53 @@
-import s from './Footer.module.css';
+'use client';
+import styles from './Footer.module.css';
+import Image from 'next/image';
+import RSLogo from '../../../public/icons/rs-logo.png';
 
-export default function Footer() {
-  return <footer className={s.footer}>Footer...</footer>;
-}
+type Author = {
+  name: string;
+  github: string;
+};
+
+const authors: Author[] = [
+  {
+    name: 'Yury Butskevich',
+    github: 'https://github.com/YourunB',
+  },
+  {
+    name: 'Liza Basarab',
+    github: 'https://github.com/Lilo002',
+  },
+  {
+    name: 'Valeryia Herasimenka',
+    github: 'https://github.com/valeryaosta',
+  },
+];
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.authorsSection}>
+        <p className={styles.authorsTitle}>Authors:</p>
+        <div className={styles.authorsList}>
+          {authors.map((author) => (
+            <p key={author.name} className={styles.authorItem}>
+              <a href={author.github} target="_blank" rel="noreferrer" className={styles.authorLink}>
+                {author.name}
+              </a>
+            </p>
+          ))}
+        </div>
+      </div>
+      <div className={styles.copyLogo}>
+        <div className={styles.copySection}>
+          <p>&copy; {new Date().getFullYear()}</p>
+        </div>
+        <div className={styles.logoSection}>
+          <Image src={RSLogo} alt={'RS Logo'} height={45} className={styles.rsLogo} />
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
