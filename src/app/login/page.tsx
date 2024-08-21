2a6108b5cfd602185d33648e3a3f33dc67f3fc25
Lilo002 <lisabasarab@gmail.com>
2024-08-21 13:48:32 +0300

    feat: add login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,15 +1,27 @@
+'use client';
 import s from './page.module.css';
-import { Metadata } from 'next';
 
-export const metadata: Metadata = {
-  title: 'Login page',
-  description: 'Login page GraphiQl App',
-};
+import { auth } from '@/firebase';
+import { useLayoutEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import LoginForm from '../components/forms/LoginForm';
 
-export default function PageLogin() {
+export default function LoginPage() {
+  const [user, loading] = useAuthState(auth);
+  const router = useRouter();
+  
+
+  useLayoutEffect(() => {
+    console.log(loading)
+    if (user) router.push('/');
+  }, [loading, router, user]);
+
+  
+
   return (
-    <main className={s.page} data-testid="child">
-      Login page...
+    <main className={s.login} data-testid="child">
+      {!loading && !user && <LoginForm/>}
     </main>
   );
 }
