cc4abfde1694c8aa3c9bf1661abe194c2c354ec4
yourunb <bxyura@gmail.com>
2024-08-26 22:48:49 +0300

    feat: create separate function for checkAuth to pages

--- src/app/[locale]/graph/page.tsx
+++ src/app/[locale]/graph/page.tsx
@@ -1,23 +1,12 @@
-'use client';
 import s from './page.module.css';
-
-import { auth } from '@/firebase';
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-import { useAuthState } from 'react-firebase-hooks/auth';
+import checkAuth from '@/app/utils/checkAuth';
 
 export default function GraphPage() {
-  const [user, loading] = useAuthState(auth);
-  const router = useRouter();
-
-  useEffect(() => {
-    if (loading) return;
-    if (!user) router.push('/');
-  }, [loading, router, user]);
+  checkAuth();
 
   return (
     <main className={s.page}>
-      страница graph
+      Graph page...
     </main>
   );
 }
