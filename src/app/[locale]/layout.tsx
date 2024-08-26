42694bc1a3de62b3b2fff37fde1fcd273563b144
valeryaosta <gerasimenko.valery@gmail.com>
2024-08-26 11:00:19 +0300

    fix: remove commented code

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,16 +1,10 @@
 'use client';
-// import type { Metadata } from 'next';
 import '../globals.css';
 import GraphAnimation from '../components/GraphAnimation';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import { useEffect } from 'react';
 import i18n from '@/i18n';
-
-// export const metadata: Metadata = {
-//   title: 'GraphiQL App',
-//   description: 'GraphiQL App student project',
-// };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   useEffect(() => {
