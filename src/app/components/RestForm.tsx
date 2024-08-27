96810ddafd49914dc91207bcd901f9df2d6866d0
yourunb <bxyura@gmail.com>
2024-08-27 21:23:17 +0300

    feat: add rest api and get data from rest page

--- src/app/components/RestForm.tsx
+++ src/app/components/RestForm.tsx
@@ -1,13 +1,103 @@
 'use client'
-import s from './GraphForm.module.css';
+import s from './RestForm.module.css';
 import checkAuth from '../utils/checkAuth';
+import React from 'react';
+import CodeMirror from '@uiw/react-codemirror';
+import { javascript } from '@codemirror/lang-javascript';
+import { githubDark } from '@uiw/codemirror-theme-github'
+import { useRef, useCallback, useState } from 'react';
+import { formatCode } from '../utils/formatCode';
+import { getDataRestApi } from '../modules/api';
 
 export default function RestForm() {
+  const inputRef = useRef(null);
+  const resultCodeMirrorRef = useRef(null);
+  const [showVariables, setShowVariables] = useState(false);
+  const [showHeaders, setShowHeaders] = useState(false);
+  const [queryValue, setQueryValue] = useState('');
+  const [variablesValue, setVariablesValue] = useState('');
+  const [headersValue, setHeadersValue] = useState('');
+  const [resultValue, setResultValue] = useState('');
+
+  const onChangeQuery = useCallback((val, viewUpdate) => {
+    console.log(viewUpdate);
+    setQueryValue(val);
+  }, []);
+
+  const onChangeVariables = useCallback((val, viewUpdate) => {
+    console.log(viewUpdate);
+    setVariablesValue(val);
+  }, []);
+
+  const onChangeHeaders = useCallback((val, viewUpdate) => {
+    console.log(viewUpdate);
+    setHeadersValue(val);
+  }, []);
+
+  const onChangeResult = useCallback((val, viewUpdate) => {
+    console.log(viewUpdate);
+    setResultValue(val);
+  }, []);
+  
+  const toggleShowVariables = () => {
+    setShowVariables(!showVariables);
+    if (showHeaders) setShowHeaders(!showHeaders);
+  }
+
+  const toggleShowHeaders = () => {
+    setShowHeaders(!showHeaders);
+    if (showVariables) setShowVariables(!showVariables);
+  }
+
   checkAuth();
+
+  const format = async (code: string, type: string, area: string) => {
+    if (code && type) {
+      const resultFormat = await formatCode(code, type);
+      if (typeof resultFormat === 'string') {
+        if (area === 'query') setQueryValue(resultFormat);
+        if (area === 'variables') setVariablesValue(resultFormat);
+        if (area === 'headers') setHeadersValue(resultFormat);
+        if (area === 'result') setResultValue(resultFormat);
+      }
+    }
+  }
+
+  const formatAllAreas = () => {
+    format(queryValue, 'graphql', 'query');
+    format(variablesValue, 'json', 'variables');
+    format(headersValue, 'json', 'headers');
+  }
+
+  const loadDataFromApi = async () => {
+    const data = await getDataRestApi(inputRef.current.value, queryValue, variablesValue, headersValue);
+    const result = JSON.stringify(data);
+    format(result, 'json', 'result');
+  }
 
   return (
     <div className={s['rest-form']}>
-      Rest form...
+      <div className={s.top}>
+        <input className={s['top__input']} defaultValue={'https://rickandmortyapi.com/api/character'} ref={inputRef} placeholder='Base URL...'/>
+        <button className={s['top__btn']} title='Format Code' onClick={() => formatAllAreas()}>&#182;</button>
+        <button className={s['top__btn']} title='Submit Endpoint'>&#8226;</button>
+        <button className={s['top__btn']} title='Execute Query' onClick={() => loadDataFromApi()}>&#10003;</button>
+      </div>
+      
+      <div className={s.box}>
+        <div className={s['form-left']}>
+          <CodeMirror value={queryValue} height="600px" extensions={[javascript({ jsx: true })]} onChange={onChangeQuery} theme={githubDark} />
+          <button className={`${s['btn-toggle']} ${showVariables ? s['btn-toggle_active'] : ''}`} onClick={() => toggleShowVariables()}>Variables</button><button className={`${s['btn-toggle']} ${showHeaders ? s['btn-toggle_active'] : ''}`} onClick={() => toggleShowHeaders()}>Headers</button>
+          {showVariables ? <CodeMirror value={variablesValue} height="200px" extensions={[javascript({ jsx: true })]} onChange={onChangeVariables} theme={githubDark} /> : null}
+          {showHeaders ? <CodeMirror value={headersValue} height="200px" extensions={[javascript({ jsx: true })]} onChange={onChangeHeaders} theme={githubDark} /> : null}
+        </div>
+
+        <div className={s['form-right']}>
+          <CodeMirror ref={resultCodeMirrorRef} value={resultValue} height="600px" extensions={[javascript({ jsx: true })]} onChange={onChangeResult} theme={githubDark} readOnly/>
+        </div>
+      </div>
+
     </div>
   );
 }
+
--- src/app/modules/api.ts
+++ src/app/modules/api.ts
@@ -19,3 +19,27 @@ export const getDataGraphApi = (url: string, query: string, variables = {}, head
       return Promise.reject(error);
     });
 };
+
+export const getDataRestApi = (url: string, query = {}, variables = {}, headers = {}) => {
+  const queryString = new URLSearchParams({ ...query, ...variables }).toString();
+  const fullUrl = `${url}?${queryString}`;
+
+  return fetch(fullUrl, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      ...headers,
+    },
+  })
+    .then(response => {
+      if (!response.ok) {
+        return Promise.reject(new Error('Failed to fetch data'));
+      }
+      return response.json();
+    })
+    .then(data => data)
+    .catch(error => {
+      console.error('Error:', error);
+      return Promise.reject(error);
+    });
+};
