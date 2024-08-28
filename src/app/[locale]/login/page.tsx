895fcc5a8f11e22315ae99b6b1ebbaa9f3ae3895
yourunb <bxyura@gmail.com>
2024-08-28 19:39:19 +0300

    feat: add metadata to login page

--- src/app/[locale]/login/page.tsx
+++ src/app/[locale]/login/page.tsx
@@ -1,6 +1,12 @@
 import s from './page.module.css';
 
 import LoginForm from '../../components/forms/LoginForm';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Login',
+  description: 'Login page',
+};
 
 export default function LoginPage() {
 
