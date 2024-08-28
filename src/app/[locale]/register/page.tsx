d78f34e5e9b594086e55d99118cba5aec9769935
yourunb <bxyura@gmail.com>
2024-08-28 19:44:17 +0300

    feat: add metadata to register page

--- src/app/[locale]/register/page.tsx
+++ src/app/[locale]/register/page.tsx
@@ -1,6 +1,12 @@
 import s from './page.module.css';
 
 import RegisterForm from '../../components/forms/RegisterForm';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Register',
+  description: 'Register page',
+};
 
 export default function RegisterPage() {
 
