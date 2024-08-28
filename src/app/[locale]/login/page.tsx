39ae93eb8fbe9f480dd6e0027a91f5f962e131b7
yourunb <bxyura@gmail.com>
2024-08-28 19:37:09 +0300

    refactor: move use client only to login form

--- src/app/[locale]/login/page.tsx
+++ src/app/[locale]/login/page.tsx
@@ -1,23 +1,12 @@
-'use client';
 import s from './page.module.css';
 
-import { auth } from '@/firebase';
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-import { useAuthState } from 'react-firebase-hooks/auth';
 import LoginForm from '../../components/forms/LoginForm';
 
 export default function LoginPage() {
-  const [user, loading] = useAuthState(auth);
-  const router = useRouter();
-
-  useEffect(() => {
-    if (user) router.push('/');
-  }, [loading, router, user]);
 
   return (
     <main className={s.login} data-testid="child">
-      {!loading && !user && <LoginForm />}
+      <LoginForm />
     </main>
   );
 }
--- src/app/components/forms/LoginForm.tsx
+++ src/app/components/forms/LoginForm.tsx
@@ -1,5 +1,6 @@
 'use client';
 import s from './LoginForm.module.css';
+
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { loginSchema } from '@/app/utils/validation';
@@ -7,8 +8,19 @@ import Link from 'next/link';
 import { logInWithEmailAndPassword } from '@/firebase';
 import { LoginData } from '@/app/type';
 import { useTranslation } from 'react-i18next';
+import { auth } from '@/firebase';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import { useAuthState } from 'react-firebase-hooks/auth';
 
 export default function LoginForm() {
+  const [user, loading] = useAuthState(auth);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (user) router.push('/');
+  }, [loading, router, user]);
+  
   const { t } = useTranslation();
   const {
     register,
@@ -28,47 +40,49 @@ export default function LoginForm() {
   };
 
   return (
-    <form className={s['login__form']} onSubmit={handleSubmit(submitFrom)}>
-      <div className={s['login__content']}>
-        <div className={s['login__field']}>
-          <label htmlFor="email" className={s['login__label']}>
-            {t('fields.email.name')}:
-          </label>
-          <input
-            {...register('email')}
-            type="text"
-            className={s['login__input']}
-            placeholder={t('fields.email.placeholder')}
-            id="email"
-          />
+    !loading && !user ? (
+      <form className={s['login__form']} onSubmit={handleSubmit(submitFrom)}>
+        <div className={s['login__content']}>
+          <div className={s['login__field']}>
+            <label htmlFor="email" className={s['login__label']}>
+              {t('fields.email.name')}:
+            </label>
+            <input
+              {...register('email')}
+              type="text"
+              className={s['login__input']}
+              placeholder={t('fields.email.placeholder')}
+              id="email"
+            />
+          </div>
+          {errors.email && <div className={s.error}>{errors.email.message}</div>}
         </div>
-        {errors.email && <div className={s.error}>{errors.email.message}</div>}
-      </div>
-      <div className={s['login__content']}>
-        <div className={s['login__field']}>
-          <label htmlFor="password" className={s['login__label']}>
-            {t('fields.password.name')}:
-          </label>
-          <input
-            {...register('password')}
-            type="password"
-            className={s['login__input']}
-            placeholder={t('fields.password.placeholder')}
-            id="password"
-          />
+        <div className={s['login__content']}>
+          <div className={s['login__field']}>
+            <label htmlFor="password" className={s['login__label']}>
+              {t('fields.password.name')}:
+            </label>
+            <input
+              {...register('password')}
+              type="password"
+              className={s['login__input']}
+              placeholder={t('fields.password.placeholder')}
+              id="password"
+            />
+          </div>
+          {errors.password && <div className={s.error}>{errors.password.message}</div>}
         </div>
-        {errors.password && <div className={s.error}>{errors.password.message}</div>}
-      </div>
-
-      <div className={s['login__btns']}>
-        <button disabled={!isValid} className={s['login__btn']} type="submit">
-          {t('login')}:
-        </button>
-      </div>
-      <div className={s['login__link']}>
-        {t('doNotHaveAccount')}? <Link href="/register">{t('register')}</Link>
-        {t('now')}.
-      </div>
-    </form>
+      
+        <div className={s['login__btns']}>
+          <button disabled={!isValid} className={s['login__btn']} type="submit">
+            {t('login')}:
+          </button>
+        </div>
+        <div className={s['login__link']}>
+          {t('doNotHaveAccount')}? <Link href="/register">{t('register')}</Link>
+          {t('now')}.
+        </div>
+      </form>
+    ) : <></>
   );
 }
