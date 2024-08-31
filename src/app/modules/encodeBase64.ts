69f3a193cae951eca3ea68dc1814538e7c7c054a
Lilo002 <lisabasarab@gmail.com>
2024-08-31 17:28:47 +0300

    feta: add fecode func

--- src/app/modules/encodeBase64.ts
+++ src/app/modules/encodeBase64.ts
@@ -1,3 +1,7 @@
 export const encodeBase64 = (str: string) => {
   return btoa(unescape(encodeURIComponent(str)));
 };
+
+export const decodeBase64 = (str: string) => {
+  return decodeURIComponent(escape(atob(str)));
+};
