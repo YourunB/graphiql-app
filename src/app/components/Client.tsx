23c9ceb45db984e75cbb83c7e4424785b4387fea
yourunb <bxyura@gmail.com>
2024-09-08 17:00:18 +0300

    feat: add Client for change forms and add 404 page

---
+++ src/app/components/Client.tsx
@@ -0,0 +1,34 @@
+'use client';
+import s from './Client.module.css';
+
+import GraphForm from './GraphForm';
+import RestForm from './RestForm';
+import { usePathname, useRouter } from 'next/navigation';
+
+const Client = () => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const parts = pathname.split('/');
+
+  let codeForm;
+  console.log(parts)
+  if (parts.length > 1 && (parts[1].toLowerCase() === 'rest' || parts[1].toLowerCase() === 'get' || parts[1].toLowerCase() === 'post' || parts[2].toLowerCase() === 'rest' || parts[2].toLowerCase() === 'get' || parts[2].toLowerCase() === 'post')) {
+    codeForm = <>
+      <h2 className={s['title']}>REST</h2>
+      <RestForm />
+    </>
+  } else if (parts.length > 1 && (parts[1].toLowerCase() === 'graph' || parts[2].toLowerCase() === 'graph')) {
+    codeForm = <>
+      <h2 className={s['title']}>GraphQL</h2>
+      <GraphForm />
+    </>
+  } else router.push('/404');
+
+  return (
+    <>
+      {codeForm}
+    </>
+  );
+};
+
+export default Client;
