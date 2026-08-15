ed44caeb9dc709cbe4f5b4dbfdf816eac54c0cd6
yourunb <bxyura@gmail.com>
2026-08-15 12:53:23 +0300

    refactor: disable pages indexation

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,3 +1,4 @@
+import { Metadata } from 'next';
 import '../globals.css';
 import GraphAnimation from '../components/GraphAnimation';
 import Footer from '../components/Footer';
@@ -5,6 +6,17 @@ import Header from '../components/Header';
 import { ErrorProvider } from '../components/error/ErrorProvider';
 import Error from '../components/error/Error';
 import I18nInitializer from '../modules/i18nInitializer';
+
+export const metadata: Metadata = {
+  robots: {
+    index: false,
+    follow: false,
+    googleBot: {
+      index: false,
+      follow: false,
+    },
+  },
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
